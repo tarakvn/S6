ee7eae6e8366fa0faf32a56a18dfe7f0a6944448
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:25:07 +0430

    Add project files.

---
+++ S6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            double square = Math.Sqrt(N);
+            double power = Math.Pow(N, 2);
+            Console.WriteLine("Squarad number: " + square);
+            Console.WriteLine("Number to the power of two: " + power);
+            Console.ReadKey();
+        }
+    }
+}
